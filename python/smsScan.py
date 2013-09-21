2690c9d12ccda6d47f2f3fa67d271a933d4a909d
BeckLi <zeyuan.sea@gmail.com>
2013-09-21 15:45:09 +0800

    完成了基本功能，可以从带BOM头的csv文件中读取短信，生成json文件

--- python/smsScan.py
+++ python/smsScan.py
@@ -1,5 +1,5 @@
 
-#coding=UTF-8
+#coding=utf-8
 import os
 import codecs
 import re
@@ -11,11 +11,33 @@ class Message():
 	def __init__(self):
 		self.type = 'RECV'
 		self.phoneno = '0'
+		self.name = ''
+		self.time = ''
 		self.content = ''
 		self.answer = ''
 
-def myFunction():
-	print "xxadafsdf"
+def toJsonString(msgs):
+	allStr = '[\n'
+	for msg in msgs:
+		allStr += '{'
+		allStr += 'phone : '
+		allStr += ('"'+msg.phoneno+'", ')
+		allStr += 'time : '
+		allStr += ('"'+msg.time+'", ')
+		allStr += 'answer : '
+		allStr += ('"'+msg.answer+'"')
+		allStr += '}'
+
+		if msg != msgs[-1]:
+			allStr += ','
+			
+		allStr += '\n'
+	allStr += ']'
+	return allStr
+
+# to do
+def retrieveAnswer(content):
+	return content
 
 def readFile(name, msgs):
 	oFile = codecs.open(name, 'r', 'utf-8-sig')
@@ -26,11 +48,25 @@ def readFile(name, msgs):
 	for line in lines:
 		#if i==0: continue
 		#print 'aaaa'
+
 		if not re.search('^sms,deliver,', line):
+			print "line:"+line + ' not match ^sms,deleiver'
+			continue
+		m = re.match('sms,deliver,(.+),(.+),,(.+,\d{3}),(.+)', line)
+
+		if not m:
+			print "line:"+line + ' not match groups'
 			continue
 
 		msg = Message()
-		msg.content = line
+		msg.phoneno = m.group(1)
+		msg.name = m.group(2)
+		msg.time = m.group(3)
+		msg.content = m.group(4)
+		msg.answer = retrieveAnswer(msg.content)
+		
+		msg.phoneno = re.sub('["\t]|\+86', '', msg.phoneno)
+
 		msgs.append(msg)
 		#print msg.content.encode('utf-8')
 		i = i+1
@@ -44,8 +80,5 @@ def readFile(name, msgs):
 """
 def writeFile(name, msgs):
 	oFile = codecs.open(name, 'w', 'utf-8')
-
-	for msg in msgs:
-		oFile.write(msg.content)
-	#oFile.write(os.linesep)
+	oFile.write(toJsonString(msgs))
 	oFile.close()
--- python/testsms.py
+++ python/testsms.py
@@ -1,4 +1,4 @@
-#codeing=UTF-8
+#coding=utf-8
 
 from smsScan import *
 
@@ -9,9 +9,21 @@ msg1.content = 'hasdhahhsdfa'
 msgs = []
 
 
-#readFile(r'sms.csv', msgs)
+readFile(r'../sms.csv', msgs)
 
-#writeFile(r'sms.json', msgs)
+writeFile(r'../sms.json', msgs)
+"""
+s = 'sms,deliver,"	+8613818908666",Cherry,,2013. 2.17 23:53,430,各种功能都试一遍啊'
+print s.decode('utf-8').encode('gbk')
+lst = re.split(',\t?', s)
+for seg in lst:
+	print seg.decode('utf-8').encode('gbk')
 
-str = 'sms,deliver,	+8613817206888,cherry,,2013. 2.17 23:53,430,asdasd'
-print re.split(',\t?', str)
+print re.sub('["\t]|\+86', '', lst[2]).decode('utf-8').encode('gbk')
+
+m = re.match('sms,deliver,.+,.+,,.+,\d{3},(.+)', s)
+if m:
+	print m.group(1).decode('utf-8').encode('gbk').strip()
+
+print 'abc\n\n\n'.strip()
+"""
